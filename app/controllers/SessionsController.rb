93d164e1cd74c279ab7a98a615317d8507bcf334
Diane Decourt <dianedecourt@gmail.com>
2020-10-29 22:47:44 +0100

    update navbar

--- app/controllers/SessionsController.rb
+++ app/controllers/SessionsController.rb
@@ -4,10 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    user = User.find_by(email:params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "You login successfully!"
+      redirect_to root_path
+    else 
+      flash.now[:alert] = 'Invalid email/password combinaison'
+    render 'new'
+    end
   end
 
   def destroy
+    session.delete(:user_id)
+    redirect_to root_path
   end
 
   def update
